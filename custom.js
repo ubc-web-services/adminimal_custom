4458541fb7a5ceefeae0e7a7771e283b673b090b
nandini <nandini.bulusu@ubc.ca>
2019-10-15 11:16:54 -0700

    Adding Adminimal Custom info

---
+++ custom.js
@@ -0,0 +1,8 @@
+(function($) {
+
+    $(document).ready(function() {
+	//disable the public option for webform upload destination
+	$('#webform-component-edit-form #edit-extra-scheme-public').attr('disabled', 'disabled');
+    });
+
+})(jQuery);
